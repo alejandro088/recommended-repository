e06f6c8020937082dd995e0f41640b5a35ec80ad
Alejandro Cayssials <admin@webxander.com>
2021-06-05 01:14:44 -0300

    Add About Container

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,21 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+    <div class="about">
+        <v-container>
+            <h1>This is an about page</h1>
+
+            <v-row  align="center"> 
+                <v-col cols="3"></v-col>
+                <v-col cols="6">
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Provident, vero? Aut expedita veritatis saepe dolorum
+                        dolores porro voluptatibus quod numquam, tempora amet
+                        molestias cupiditate aliquid! Quae repellat ipsam dolore
+                        ipsa.
+                    </p>
+                </v-col>
+                <v-col cols="3"></v-col>
+            </v-row>
+        </v-container>
+    </div>
 </template>
