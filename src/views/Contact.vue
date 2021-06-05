336dd4dd8b806eb0bdc57bcbc1a03f56b22ef209
Alejandro Cayssials <admin@webxander.com>
2021-06-05 00:57:14 -0300

    Add contact form

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,5 +1,72 @@
 <template>
-  <div class="about">
-    <h1>This is an contact page</h1>
-  </div>
+    <div class="contact">
+        <h1>This is an contact page</h1>
+
+        <v-form v-model="valid">
+            <v-container>
+                <v-row>
+                    <v-col cols="12" md="4">
+                        <v-text-field
+                            v-model="firstname"
+                            :rules="nameRules"
+                            :counter="10"
+                            label="First name"
+                            required
+                        ></v-text-field>
+                    </v-col>
+
+                    <v-col cols="12" md="4">
+                        <v-text-field
+                            v-model="lastname"
+                            :rules="nameRules"
+                            :counter="10"
+                            label="Last name"
+                            required
+                        ></v-text-field>
+                    </v-col>
+
+                    <v-col cols="12" md="4">
+                        <v-text-field
+                            v-model="email"
+                            :rules="emailRules"
+                            label="E-mail"
+                            required
+                        ></v-text-field>
+                    </v-col>
+                </v-row>
+
+                <v-row>
+                    <v-textarea
+                        autocomplete="message"
+                        label="Message"
+                    ></v-textarea>
+                </v-row>
+
+                <v-row>
+                    <v-btn block>
+                        Enviar
+                    </v-btn>
+                </v-row>
+            </v-container>
+        </v-form>
+    </div>
 </template>
+
+<script>
+export default {
+    data: () => ({
+        valid: false,
+        firstname: '',
+        lastname: '',
+        nameRules: [
+            (v) => !!v || 'Name is required',
+            (v) => v.length <= 10 || 'Name must be less than 10 characters',
+        ],
+        email: '',
+        emailRules: [
+            (v) => !!v || 'E-mail is required',
+            (v) => /.+@.+/.test(v) || 'E-mail must be valid',
+        ],
+    }),
+};
+</script>
