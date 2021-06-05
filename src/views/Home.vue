fdd67ba4f44580763a5c4fe67ebd2e7b6b2d8f8b
Alejandro Cayssials <admin@webxander.com>
2021-06-04 22:36:27 -0300

    Add description

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,10 @@
   <div class="home">
     <img alt="Logo" src="https://image.freepik.com/free-vector/web-development-programmer-engineering-coding-website-augmented-reality-interface-screens-developer-project-engineer-programming-software-application-design-cartoon-illustration_107791-3863.jpg">
 
+    <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius deserunt, modi ab velit delectus impedit. Tenetur cum libero natus. Nostrum nobis quisquam</h2>
 
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita cumque aperiam maxime aliquam, amet animi molestias laboriosam rerum corporis, minima laudantium autem temporibus sint harum consequatur tempore. Aut, recusandae laboriosam.</p>
+    
   </div>
 </template>
 
