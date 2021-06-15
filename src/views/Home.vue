e8b7cde76e41f98b04a6e805907608b2b3b2121a
Alejandro Cayssials <admin@webxander.com>
2021-06-15 19:47:21 -0300

    update title

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,6 +6,8 @@
                 src="https://image.freepik.com/free-vector/web-development-programmer-engineering-coding-website-augmented-reality-interface-screens-developer-project-engineer-programming-software-application-design-cartoon-illustration_107791-3863.jpg"
             />
 
+            <h1> Greetings FrontEnd Cafe!! </h1>
+
             <h2>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius
                 deserunt, modi ab velit delectus impedit. Tenetur cum libero
