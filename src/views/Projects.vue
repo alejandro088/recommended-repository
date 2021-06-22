34e55aa028bab2275fc14fe64531bb97ca013995
Alejandro Cayssials <admin@webxander.com>
2021-06-22 19:51:12 -0300

    add footer component

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -140,11 +140,16 @@
                 </v-card>
             </v-row>
         </v-container>
+
+         <Footer />
     </div>
 </template>
 
 <script>
+import Footer from '@/components/Footer';
+
 export default {
+    components: { Footer },
     data: () => ({
         show: false,
     }),
