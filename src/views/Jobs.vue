eddc93d8bec25815444f1dda6eb396e8a1ccc864
Alejandro Cayssials <admin@webxander.com>
2021-06-27 23:42:35 -0300

    add Jobs

---
+++ src/views/Jobs.vue
@@ -0,0 +1,106 @@
+<template>
+    <div class="about">
+        <v-container>
+            <h1>This is an about page</h1>
+
+            <v-row align="center">
+                <v-col cols="3"></v-col>
+                <v-col cols="6">
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Provident, vero? Aut expedita veritatis saepe dolorum
+                        dolores porro voluptatibus quod numquam, tempora amet
+                        molestias cupiditate aliquid! Quae repellat ipsam dolore
+                        ipsa.
+                    </p>
+                </v-col>
+                <v-col cols="3"></v-col>
+            </v-row>
+        </v-container>
+
+        <v-container>
+            <h1>About Us</h1>
+
+            <v-row align="center">
+                <v-col>
+                    <v-row>
+                    <v-avatar>
+                        <img
+                            src="https://cdn.vuetifyjs.com/images/john.jpg"
+                            alt="John"
+                        />
+                    </v-avatar>
+                    </v-row>
+                    <v-row align-content="center">
+                    Project Manager
+                    </v-row>
+                </v-col>
+                <v-col>
+                   <v-row>
+                    <v-avatar>
+                        <img
+                            src="https://cdn.vuetifyjs.com/images/john.jpg"
+                            alt="John"
+                        />
+                    </v-avatar>
+                    </v-row>
+                    <v-row align-content="center">
+                    Team Manager
+                    </v-row>
+                </v-col>
+                <v-col>
+                   <v-row>
+                    <v-avatar>
+                        <img
+                            src="https://cdn.vuetifyjs.com/images/john.jpg"
+                            alt="John"
+                        />
+                    </v-avatar>
+                    </v-row>
+                    <v-row align-content="center">
+                    Backend Developer
+                    </v-row>
+                </v-col>
+                <v-col>
+                    <v-row>
+                    <v-avatar>
+                        <img
+                            src="https://cdn.vuetifyjs.com/images/john.jpg"
+                            alt="John"
+                        />
+                    </v-avatar>
+                    </v-row>
+                    <v-row align-content="center">
+                    Backend Developer
+                    </v-row>
+                </v-col>
+                <v-col>
+                    <v-row>
+                    <v-avatar>
+                        <img
+                            src="https://cdn.vuetifyjs.com/images/john.jpg"
+                            alt="John"
+                        />
+                    </v-avatar>
+                    </v-row>
+                    <v-row align-content="center">
+                    FrontEnd Developer
+                    </v-row>
+                </v-col>
+                <v-col>
+                    <v-row>
+                    <v-avatar>
+                        <img
+                            src="https://cdn.vuetifyjs.com/images/john.jpg"
+                            alt="John"
+                        />
+                    </v-avatar>
+                    </v-row>
+                    <v-row align-content="center">
+                    Team QA
+                    </v-row>
+                </v-col>
+            </v-row>
+        </v-container>
+    </div>
+</template>
